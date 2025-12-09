3ed22fc24a0edffba27f064df93947a8b4f48cd5
Inneren12 <chulkov.oleksii.can@gmail.com>
2025-12-08 20:02:35 -0500

    Add android test runner deps to core-data

--- core-data/build.gradle.kts
+++ core-data/build.gradle.kts
@@ -80,6 +80,8 @@ dependencies {
   androidTestImplementation("androidx.room:room-testing:2.6.1")
   androidTestImplementation("androidx.test:core-ktx:1.5.0")
   androidTestImplementation("androidx.test.ext:junit:1.1.5")
+  androidTestImplementation("androidx.test:runner:1.5.2")
+  androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
   androidTestImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.8.1")
 
     implementation("com.jakewharton.timber:timber:5.0.1")
